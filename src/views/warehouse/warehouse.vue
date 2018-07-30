03886e62d3c71f5f2d7692c2cb34a98ba9a4a714
qingzhong <qingzhong.feng@unisco.com>
2018-07-30 15:58:22 +0800

    2018-7-18

--- src/views/warehouse/warehouse.vue
+++ src/views/warehouse/warehouse.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
     .loding-bader
+        <!--div {{searchInfo.phone}}-->
         <!--ui-select-choices.input(:isFocus="true" v-bind:selectData.sync="searchInfo.phone" v-bind:deselectData="setPhoneSelect")-->
+        div {{searchInfo.email}}
         input-select-choices.input-select(:isFocus="true" v-bind:selectData.sync="searchInfo.email" v-bind:deselectData="setEmailSelect")
 
 </template>
@@ -12,7 +14,7 @@
     .input
         fz(24px); width 60%; margin auto;
     .input-select
-        fz(24px);$mar(l 75px no,r 75px no,t 30px no); background #48ccbf;height 40px;order-radius 6px;/*no*/
+        fz(24px);$mar(l 75px no,r 75px no,t 30px no);height 40px;order-radius 6px;/*no*/
     .divtext
         $wh(w 500px,h 200px); background rgba(12, 204, 91, 0.49);
     .dd
