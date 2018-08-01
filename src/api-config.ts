cf95024eeb6fd71f54d3cbaaef91e203062bc2fe
qingzhong <qingzhong.feng@unisco.com>
2018-08-01 17:29:07 +0800

    2018-08-17

--- src/api-config.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by f on 2018/5/2.
- */
-const apiConfig={
-  baseURL: 'http://www.dashserver.com',
-  timeout: 300
-}
-export default apiConfig;
--- src/httpAnency/index.ts
+++
@@ -1,6 +0,0 @@
-import { Vue } from "vue-property-decorator";
-import rxax from './rxToaxios'
-export default class axVue extends Vue{
-  apis$=rxax
-
-}
